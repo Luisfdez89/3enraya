cbcca3e8debbba405b5028ca8b4212e3b5b87f77
Luisfdez89 <Luisfdezgarcia7@gmail.com>
2024-05-08 18:43:47 +0200

    v2

--- 3enraya.py
+++ 3enraya.py
@@ -1,94 +1,103 @@
-CELDAS = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+CELDAS = ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G']
+
 X, O, BLANCO = 'X', 'O', ' '
 
 
 def main():
-    print('Bienvenido a 3 en raya!')
-    tableroJuego = obtenerTableroVacio()  # Crea el tablero vacío de 3 en raya
-    jugadorActual, jugadorSiguiente = X, O  # X primero, O siguiente
+    print('Benvingut a 4 en ratlla!')
+    tableroJuego = obtenerTableroVacio()  # Crea el tablero vacío de 4 en ratlla
+    jugadorActual, jugadorSiguiente = X, O  # X primer, O següent
 
-    while True:  # Bucle principal del juego
-        # Muestra el tablero en pantalla:
+    while True:  # Bucle principal del joc
+        # Mostra el tauler a la pantalla:
         print(obtenerStrTablero(tableroJuego))
 
-        # pregunta mientras el usuario no introduzca una celda válida
+        # Pregunta mentre l'usuari no introdueixi una cel·la vàlida
         movimiento = None
         while not esCeldaValida(tableroJuego, movimiento):
             print('')
-            print('Cuál es el movimiento de {}? (1-9)'.format(jugadorActual))
+            print('Quin és el moviment de {}? (1-D)'.format(jugadorActual))
             movimiento = input('> ')
 
         tableroJuego[movimiento] = jugadorActual
 
-        # Comprueba si el juego ha terminado
-        if esGanador(tableroJuego, jugadorActual):  # Comprueba si hay ganador
+        # Comprova si el joc ha acabat
+        if esGanador(tableroJuego, jugadorActual):  # Comprova si hi ha guanyador
             print(obtenerStrTablero(tableroJuego))
             print('')
-            print(jugadorActual + ' ha ganado la partida!')
+            print(jugadorActual + ' ha guanyat la partida!')
             break
-        elif tableroLleno(tableroJuego):  # Comprueba si hay empate
+        elif tableroLleno(tableroJuego):  # Comprova si hi ha empat
             print(obtenerStrTablero(tableroJuego))
             print('')
-            print('Empate!')
+            print('Empat!')
             break
 
-        # Cambio de turno entre jugadores:
+        # Canvi de torn entre jugadors:
         jugadorActual, jugadorSiguiente = jugadorSiguiente, jugadorActual
 
-    print('Gracias por jugar!')
+    print('Gràcies per jugar!')
 
 
 def obtenerTableroVacio():
-    """Crea un tablero vacío para la partida de 3 en raya."""
-    # Mapeado : 1|2|3
-    #           -+-+-
-    #           4|5|6
-    #           -+-+-
-    #           7|8|9
+    """Crea un tauler buit per al joc de 4 en ratlla."""
+    # Mapeig: 1|2|3|4
+    #         -+-+-
+    #         5|6|7|8
+    #         -+-+-
+    #         9|A|B|C
+    #         -+-+-
+    #         D|E|F|G
     tablero = {}
     for celda in CELDAS:
-        tablero[celda] = BLANCO  # Todas las celdas del tablero se inicializan a BLANCO
+        tablero[celda] = BLANCO  # Totes les cel·les del tauler s'inicialitzen a BLANCO
     return tablero
 
 
 def obtenerStrTablero(tablero):
-    """Devuelve el tablero formateado como cadena de texto."""
+    """Retorna el tauler formatat com a cadena de text."""
     return '''
-      {}|{}|{}  1 2 3
+      {}|{}|{}|{}  1 2 3 4
       -+-+-
-      {}|{}|{}  4 5 6
+      {}|{}|{}|{}  5 6 7 8
       -+-+-
-      {}|{}|{}  7 8 9'''.format(tablero['1'], tablero['2'], tablero['3'],
-                                tablero['4'], tablero['5'], tablero['6'],
-                                tablero['7'], tablero['8'], tablero['9'])
+      {}|{}|{}|{}  9 A B C
+      -+-+-
+      {}|{}|{}|{}  D E F G'''.format(tablero['1'], tablero['2'], tablero['3'], tablero['4'],
+                                     tablero['5'], tablero['6'], tablero['7'], tablero['8'],
+                                     tablero['9'], tablero['A'], tablero['B'], tablero['C'],
+                                     tablero['D'], tablero['E'], tablero['F'], tablero['G'])
 
 
 def esCeldaValida(tablero, celda):
-    """Devuelve True si la celda es válida y está en BLANCO."""
+    """Retorna True si la cel·la és vàlida i està en BLANCO."""
     return celda in CELDAS and tablero[celda] == BLANCO
 
 
 def esGanador(tablero, jugador):
-    """Devuelve True si el jugador es ganador."""
-    # Se utilizan variables cortas para mejorar la legibilidad
+    """Retorna True si el jugador és guanyador."""
+    # S'utilitzen variables curtes per millorar la llegibilitat
     b, p = tablero, jugador
-    # Busca el 3 en raya en las 3 filas, 3 columnas y las 2 diagonales
-    return ((b['1'] == b['2'] == b['3'] == p) or  # Horizontal arriba
-            (b['4'] == b['5'] == b['6'] == p) or  # Horizontal medio
-            (b['7'] == b['8'] == b['9'] == p) or  # Horizontal abajo
-            (b['1'] == b['4'] == b['7'] == p) or  # Vertical izquierda
-            (b['2'] == b['5'] == b['8'] == p) or  # Vertical centro
-            (b['3'] == b['6'] == b['9'] == p) or  # Vertical derecha
-            (b['3'] == b['5'] == b['7'] == p) or  # Diagonal
-            (b['1'] == b['5'] == b['9'] == p))  # Diagonal
+    # Cerca el 4 en ratlla a les 4 files, 4 columnes i les 2 diagonals
+    return ((b['1'] == b['2'] == b['3'] == b['4'] == p) or  # Horitzontal amunt
+            (b['5'] == b['6'] == b['7'] == b['8'] == p) or  # Horitzontal mig
+            (b['9'] == b['A'] == b['B'] == b['C'] == p) or  # Horitzontal baix
+            (b['D'] == b['E'] == b['F'] == b['G'] == p) or  # Horitzontal extrem
+            (b['1'] == b['5'] == b['9'] == b['D'] == p) or  # Diagonal
+            (b['4'] == b['7'] == b['A'] == b['D'] == p) or  # Diagonal inversa
+            (b['1'] == b['6'] == b['B'] == b['G'] == p) or  # Diagonal secundària
+            (b['4'] == b['5'] == b['6'] == b['7'] == p) or  # Vertical esquerra
+            (b['2'] == b['6'] == b['A'] == b['E'] == p) or  # Vertical centre
+            (b['3'] == b['7'] == b['B'] == b['F'] == p) or  # Vertical dreta
+            (b['8'] == b['B'] == b['C'] == b['F'] == p))  # Vertical extrem
 
 
 def tableroLleno(tablero):
-    """Devuelve True si todas las celdas están ocupadas."""
+    """Retorna True si totes les cel·les estan ocupades."""
     for celda in CELDAS:
         if tablero[celda] == BLANCO:
-            return False  # Hay al menos una celda que no está en blanco por lo que devolvemos False
-    return True  # Ningún espacio en BLANCO por lo que se devuelve True
+            return False  # Hi ha almenys una cel·la que no està en blanc, per tant, retorna False
+    return True  # Cap espai en BLANCO, per tant, retorna True
 
 
 if __name__ == '__main__':
